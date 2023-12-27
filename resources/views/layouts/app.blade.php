bf8e4841c0b43cc42905029a88e5e19e6a1c9760
danielsundermeier <daniel@hof-sundermeier.de>
2023-12-27 12:33:30 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,8 +123,8 @@
 	<div class="bg-gray-50">
 		<div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
 			<h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-				<span class="block">Fragen, Anregungen oder Ideen?</span>
-				<span class="block text-sky-600">Schreib mit eine Nachricht.</span>
+				<span class="block">Was ist dein Traum?</span>
+				<span class="block text-sky-600">Wie kann ich helfen?</span>
 			</h2>
 			<div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
 				<div class="inline-flex rounded-md shadow">
