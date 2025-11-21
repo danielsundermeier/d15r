58ef97eff5f5988d04bcdcf94a6b8bacdbd37c54
danielsundermeier <daniel@hof-sundermeier.de>
2025-11-21 15:26:33 +0100

    Refactor Markdown controller to reuse response logic
    Moved shared markdown processing logic into a new protected respond() method. Both index() and show() now call respond() to reduce code duplication and improve maintainability.

--- app/Http/Controllers/MarkdownWithTocController.php
+++ app/Http/Controllers/MarkdownWithTocController.php
@@ -25,33 +25,7 @@ class MarkdownWithTocController extends Controller
 
     public function index()
     {
-        $markdown = file_get_contents(resource_path($this->markdown_path));
-        $first_line = preg_split('#\r?\n#', $markdown, 0)[0];
-        $title = trim(str_replace('# ', '', $first_line));
-        $markdown_without_title = trim(substr($markdown, strpos($markdown, "\n") + 1));
-
-        $environment = $this->getEnvironment();
-
-        $converter = new MarkdownConverter($environment);
-
-        $converted = $converter->convert($markdown_without_title);
-
-        $document = $converted->getDocument();
-
-        $toc = (new Query())
-            ->where(Query::type(TableOfContents::class))
-            ->findOne($document);
-
-        $toc->detach();
-
-        $renderer = new HtmlRenderer($environment);
-        $content = $renderer->renderDocument($document);
-        $toc = $renderer->renderNodes([$toc]);
-
-        return view('markdown-with-toc.index')
-            ->with('title', $title)
-            ->with('content', $content)
-            ->with('toc', $toc);
+        return $this->respond($this->markdown_path);
     }
 
     public function show(string $section)
@@ -60,6 +34,11 @@ class MarkdownWithTocController extends Controller
 
         abort_if(!file_exists(resource_path($markdown_path)), 404);
 
+        return $this->respond($markdown_path);
+    }
+
+    protected function respond(string $markdown_path)
+    {
         $markdown = file_get_contents(resource_path($markdown_path));
         $first_line = preg_split('#\r?\n#', $markdown, 0)[0];
         $title = trim(str_replace('# ', '', $first_line));
